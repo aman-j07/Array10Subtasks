310a4acb83851e41b48591252e675ddc46e60526
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-07-25 18:15:56 +0530

    Subtask 3

--- jsarray.js
+++ jsarray.js
@@ -5,12 +5,16 @@ details=[
     {company:"Motorola",model:"G10",memory:32,price:15000},
     {company:"Apple",model:"S12",memory:64,price:25000}
 ]
+
 populate();
 optionsel=document.getElementById("heads");
+
 searched=document.getElementById("search");
 tablesearch=document.getElementById("tablesearch");
+
 function populate(){
 list=document.getElementById("list");
+list.innerHTML="";
 text=""
 for(i=0;i<details.length;i++){
     
@@ -18,6 +22,7 @@ for(i=0;i<details.length;i++){
 }
 list.innerHTML+=text;
 }
+
 function search(){
 index=optionsel.selectedIndex;
 alert(index)
@@ -35,4 +40,75 @@ else if(index==3 && searched.value==details[i].price){
     tablesearch.innerHTML="<table id='table2'><tr><td>"+details[i].company+"</td><td>"+details[i].model+"</td><td>"+details[i].memory+"</td><td>"+details[i].price+"</td></tr></table>";
 }
 }
+}
+
+function sorting(){
+    alert();
+    sort=document.getElementById("sort");
+    indexSort=sort.selectedIndex;
+    console.log(indexSort)
+    optionsel2=document.getElementById("heads2");
+    index=optionsel2.selectedIndex;
+    console.log(index)
+    if(indexSort==1){
+        if(index==1){
+            console.log(details)
+            details.sort((a,b)=>{
+            x=a.company.toLowerCase();
+            y=b.company.toLowerCase();
+            if(x>y){return 1;}
+            if(x<y){return -1;}
+            return 0;});
+        }
+        if(index==2){
+            console.log(details)
+            details.sort((a,b)=>{
+            x=a.model.toLowerCase();
+            y=b.model.toLowerCase();
+            if(x>y){return 1;}
+            if(x<y){return -1;}
+            return 0;});
+        }
+        if(index==3){
+            details.sort((a,b)=>{return a.memory - b.memory});
+            console.log(details)
+        }
+        if(index==4){
+            details.sort((a,b)=>{return a.price - b.price});
+            console.log(details)
+    }
+    }
+
+    if(indexSort==2){
+        if(index==1){
+            console.log(details)
+            details.sort((a,b)=>{
+            x=a.company.toLowerCase();
+            y=b.company.toLowerCase();
+            if(x<y){return 1;}
+            if(x>y){return -1;}
+            return 0;});
+        }
+        if(index==2){
+            details.slice(0,details.length);
+            console.log(details)
+            detailscopy.sort((a,b)=>{
+            x=a.model.toLowerCase();
+            y=b.model.toLowerCase();
+            if(x<y){return 1;}
+            if(x>y){return -1;}
+            return 0;});
+        }
+        if(index==3){
+            details.slice(0,details.length);
+            details.sort((a,b)=>{return b.memory - a.memory});
+            console.log(details)
+        }
+        if(index==4){
+           details.slice(0,details.length);
+            details.sort((a,b)=>{return b.price - a.price});
+            console.log(details)
+    }
+}
+populate();
 }
