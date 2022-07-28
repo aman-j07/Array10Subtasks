92a31bf257920a025c7769201af0d5f0a2655734
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-07-28 15:37:36 +0530

    Subtask 4

--- jsAdvanced10.js
+++ jsAdvanced10.js
@@ -5,11 +5,34 @@ class details1{
       this.name=name;
       this.basicSal=basicSal;
     }
+    jsonAccept(jsonObj){
+     var obj=JSON.parse(jsonObj);
+     this.id =obj.id;
+     this.name=obj.name;
+     this.basicSal=obj.basicSal;
+
+
+    }
+
 }
 // let obj1=new details1(1,'John',8000);
 // console.log(obj1)
-text='{"id":"1","name":"John","basicSal":"8000"}';
-const obj2=JSON.parse(text);
+// text='"employees":['+'{"id":"1","name":"John","basicSal":"8000"},'+'{"id":"2","name":"Mark","basicSal":"9000"},'+'{"id":"3","name":"Yash","basicSal":"7000"}]';
+
+let objDetails=new details1(2,"Akash",12000); 
+jObj='{"id":"4","name":"Vikram","basicSal":"18000"}';
+objDetails.jsonAccept(jObj);
+console.log("changed object",objDetails);
+
+
+var tableDetails=document.getElementById("tableDetails");
+tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
+tableDetails.innerHTML+="<tr><td>"+objDetails.id+"</td><td>"+objDetails.name+"</td><td>"+objDetails.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
+
+ 
+
+// const details2=JSON.parse(text);
+// console.log(details2[2])
 
 
 // var tableDetails=document.getElementById("tableDetails");
@@ -17,12 +40,6 @@ const obj2=JSON.parse(text);
 // tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
 // tableDetails.innerHTML+="<tr><td>"+obj1.id+"</td><td>"+obj1.name+"</td><td>"+obj1.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
 
-var tableDetails=document.getElementById("tableDetails");
-tableDetails.innerHTML='';
-tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
-tableDetails.innerHTML+="<tr><td>"+obj2.id+"</td><td>"+obj2.name+"</td><td>"+obj2.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
-
- 
 // function add(){
 //     var id=document.getElementById("inputId").value;
 //     var name=document.getElementById("inputName").value;
