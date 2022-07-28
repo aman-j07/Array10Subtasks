2ba84457075792fe7dc030b68a721e5b047fea28
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-07-28 16:29:57 +0530

    Subtask 5

--- jsAdvanced10.js
+++ jsAdvanced10.js
@@ -1,35 +1,34 @@
-const details=[];
-class details1{
-    constructor(id,name,basicSal){
-      this.id=id;
-      this.name=name;
-      this.basicSal=basicSal;
-    }
-    jsonAccept(jsonObj){
-     var obj=JSON.parse(jsonObj);
-     this.id =obj.id;
-     this.name=obj.name;
-     this.basicSal=obj.basicSal;
-
-
-    }
-
+const details=[
+  {id:1,name:'John',basicSal:8000,updatedSal:0},
+  {id:2,name:'Mark',basicSal:9000,updatedSal:0},
+  {id:3,name:'Yash',basicSal:7000,updatedSal:0}
+];
+update();
+function update(){
+for(i=0;i<details.length;i++){
+  try{
+  details[i].updatedSal=parseInt(details[i].basicSal*1.10);
+  }
+  catch{
+    document.getElementById("para").innerHTML="Salary of "+details[i].name+"cannot be calculated";
+  }
 }
+}
+// class details1{
+//     constructor(id,name,basicSal){
+//       this.id=id;
+//       this.name=name;
+//       this.basicSal=basicSal;
+//     }
+
+// }
 // let obj1=new details1(1,'John',8000);
 // console.log(obj1)
 // text='"employees":['+'{"id":"1","name":"John","basicSal":"8000"},'+'{"id":"2","name":"Mark","basicSal":"9000"},'+'{"id":"3","name":"Yash","basicSal":"7000"}]';
 
-let objDetails=new details1(2,"Akash",12000); 
-jObj='{"id":"4","name":"Vikram","basicSal":"18000"}';
-objDetails.jsonAccept(jObj);
-console.log("changed object",objDetails);
-
-
-var tableDetails=document.getElementById("tableDetails");
-tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
-tableDetails.innerHTML+="<tr><td>"+objDetails.id+"</td><td>"+objDetails.name+"</td><td>"+objDetails.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
-
- 
+// var tableDetails=document.getElementById("tableDetails");
+// tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
+// tableDetails.innerHTML+="<tr><td>"+objDetails.id+"</td><td>"+objDetails.name+"</td><td>"+objDetails.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
 
 // const details2=JSON.parse(text);
 // console.log(details2[2])
@@ -40,19 +39,27 @@ tableDetails.innerHTML+="<tr><td>"+objDetails.id+"</td><td>"+objDetails.name+"</
 // tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
 // tableDetails.innerHTML+="<tr><td>"+obj1.id+"</td><td>"+obj1.name+"</td><td>"+obj1.basicSal+"</td><td><a href=''>Delete</a></td></tr>";
 
-// function add(){
-//     var id=document.getElementById("inputId").value;
-//     var name=document.getElementById("inputName").value;
-//     var basicSal=document.getElementById("inputBasicSal").value;
-//     var tableDetails=document.getElementById("tableDetails");
-//     tableDetails.innerHTML='';
+tableDetails.innerHTML+="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Updated Salary</th></tr>"
+for(i=0;i<details.length;i++){
+    tableDetails.innerHTML+="<tr><td>"+details[i].id+"</td><td>"+details[i].name+"</td><td>"+details[i].basicSal+"</td><td>"+details[i].updatedSal+"</td></tr>";
+}
+function add(){
+    var id=document.getElementById("inputId").value;
+    var name=document.getElementById("inputName").value;
+    var basicSal=document.getElementById("inputBasicSal").value;
+    var tableDetails=document.getElementById("tableDetails");
+    tableDetails.innerHTML='';
 
-//     var detail={id:id,name:name,basicSal:basicSal};
+    var detail={id:id,name:name,basicSal:basicSal};
+    details.push(detail);
     
 
-//     tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
-//     for(i=0;i<details.length;i++){
-//         tableDetails.innerHTML+="<tr><td>"+details[i].id+"</td><td>"+details[i].name+"</td><td>"+details[i].basicSal+"</td><td><a href=''>Delete</a></td></tr>";
-//     }
-  
-// }
+    tableDetails.innerHTML="<tr><th>Id</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
+    for(i=0;i<details.length;i++){
+        tableDetails.innerHTML+="<tr><td>"+details[i].id+"</td><td>"+details[i].name+"</td><td>"+details[i].basicSal+"</td></tr>";
+    }
+    update();
+    document.getElementById("inputId").value='';
+    document.getElementById("inputName").value='';
+    document.getElementById("inputBasicSal").value=''; 
+}
